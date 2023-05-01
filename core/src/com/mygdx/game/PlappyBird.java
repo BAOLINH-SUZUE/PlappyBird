beea0ea55abeb25244a14d5e5d28801b142ef41d
DOANLINHFFFF <119753910+DOANLINHFFFF@users.noreply.github.com>
2023-05-02 02:04:57 +0700

    add bird

--- core/src/com/mygdx/game/PlappyBird.java
+++ core/src/com/mygdx/game/PlappyBird.java
@@ -1,31 +1,64 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class PlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture background;
+	Texture[] birds;
+	int flapState= 0;
+	float birdY = 0;
+	float velocity = 0;
+	int gameState = 0;
+	float gravity = 2;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("bg.png");
+		birds = new Texture[2];
+		birds[0] = new Texture("bird.png");
+		birds[1] = new Texture("bird2.png");
+		birdY= Gdx.graphics.getHeight()/2 - birds[flapState].getHeight()/2;
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+
+		if (gameState !=0 ) {
+			if(Gdx.input.justTouched())
+			{
+				velocity = -20;
+			}
+			velocity= velocity+ gravity;
+			birdY -= velocity;
+
+
+		} else
+		{
+			if(Gdx.input.justTouched())
+			{
+				gameState = 1;
+			}
+
+		}
+
+		if (flapState == 0) {
+			flapState = 1;
+		} else
+		{
+			flapState = 0;
+		}
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//create background
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		//create bird
+		batch.draw(birds[flapState], Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2, birdY);
 		batch.end();
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
 }
