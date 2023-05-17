b64f12fce65cdf5430f36814892488c1986bf39e
DOANLINHFFFF <119753910+DOANLINHFFFF@users.noreply.github.com>
2023-05-17 19:22:30 +0700

    baolinh 17/5

--- core/src/com/mygdx/game/PlappyBird.java
+++ core/src/com/mygdx/game/PlappyBird.java
@@ -52,6 +52,7 @@ public class PlappyBird extends ApplicationAdapter {
 		//ShapeRenderer shapeRenderer= new ShapeRenderer();
 		birdcircle= new Circle();
 
+		// create bird
 		birds = new Texture[2];
 		birds[0] = new Texture("bird.png");
 		birds[1] = new Texture("bird2.png");
@@ -65,11 +66,10 @@ public class PlappyBird extends ApplicationAdapter {
 		bottomTube = new Texture("bottomtube.png");
 		maxTubeOffset = Gdx.graphics.getHeight()/2 - gap/2 - 100;
 		randomGenerator = new Random();
-		distanceBetweenTheTube = Gdx.graphics.getHeight()* 3/4;
+		//distanceBetweenTheTube = Gdx.graphics.getHeight()* 3/4;
 		topTubeRectangle = new Rectangle[numberOfTubes];
 		bottomTubeRectangles = new Rectangle[numberOfTubes];
 		startGame();
-
 
 	}
 	public void startGame()
@@ -90,101 +90,94 @@ public class PlappyBird extends ApplicationAdapter {
 		batch.begin();
 		//create background
 		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		if (gameState ==1 ) {
-
-			if(tubeX[scoringTube]< Gdx.graphics.getWidth()/2)
-			{
-				Gdx.app.log("Score",String.valueOf(score));
+		if (gameState == 1) {
+			// score
+			if (tubeX[scoringTube] < Gdx.graphics.getWidth() / 2) {
+				Gdx.app.log("Score", String.valueOf(score));
 				score++;
-				if(scoringTube<4)
-				{
+				if (scoringTube < 4) {
 					scoringTube++;
-				}else
-				{
+				} else {
 					scoringTube = 0;
 				}
 			}
-			if(Gdx.input.justTouched())
-			{
+
+
+			if (Gdx.input.justTouched()) {
 				velocity = -30;
 
-
 			}
-			for (int i = 0; i< numberOfTubes; i++) {
+			for (int i = 0; i < numberOfTubes; i++) {
 
-				if (tubeX[i]<-topTube.getWidth())
-				{
-					tubeX[i] +=numberOfTubes * distanceBetweenTheTube;
-					tubeOffset[i]= (randomGenerator.nextFloat()- 0.5f ) * (Gdx.graphics.getHeight() - gap - 200);
-				}else {
-					tubeX[i]= tubeX[i]- tubeVelocity;
-
-
-
+				if (tubeX[i] < -topTube.getWidth()) {
+					tubeX[i] += numberOfTubes * distanceBetweenTheTube;
+				//	tubeOffset[i]= (randomGenerator.nextFloat()- 0.5f ) * (Gdx.graphics.getHeight() - gap - 200);
+				} else {
+					tubeX[i] = tubeX[i] - tubeVelocity;
 				}
+				//Create bottom and toptube and align
 				batch.draw(topTube, tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i]);
 				batch.draw(bottomTube, tubeX[i], Gdx.graphics.getHeight() / 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i]);
 
-				topTubeRectangle[i]= new Rectangle(tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i], topTube.getWidth(),topTube.getHeight());
-				bottomTubeRectangles[i] = new Rectangle(tubeX[i],Gdx.graphics.getHeight() / 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i],bottomTube.getWidth(),bottomTube.getHeight() );
+
+				topTubeRectangle[i] = new Rectangle(tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i], topTube.getWidth(), topTube.getHeight());
+				bottomTubeRectangles[i] = new Rectangle(tubeX[i], Gdx.graphics.getHeight() / 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i], bottomTube.getWidth(), bottomTube.getHeight());
 			}
 
-			if(birdY>0 ) {
+			//prevent the bird from leaving the screen
+			if (birdY > 0|| velocity <0) {
 				velocity = velocity + gravity;
 				birdY -= velocity;
-			}else {
+
+			} else {
 				gameState = 2;
 			}
 
 
-		} else if(gameState==0) {
-			if(Gdx.input.justTouched())
-			{
+		} else if (gameState == 0) {
+			if (Gdx.input.justTouched()) {
 				gameState = 1;
 			}
 
 		}else  if (gameState ==2)
 		{
 
-			batch.draw(gameover , Gdx.graphics.getWidth()/2- gameover.getWidth()/2, Gdx.graphics.getHeight()/2 - gameover.getHeight());
+			batch.draw(gameover, Gdx.graphics.getWidth() / 2 - gameover.getWidth() / 2, Gdx.graphics.getHeight() / 2 - gameover.getHeight());
 			if(Gdx.input.justTouched())
 			{
 				gameState = 1;
 				startGame();
 				score= 0;
 				scoringTube= 0;
-				velocity =0;
+			velocity =0;
 			}
-		}
-
-		if (flapState == 0) {
-			flapState = 1;
-		} else
-		{
-			flapState = 0;
-		}
-
-		//create bird
-		batch.draw(birds[flapState], Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2, birdY);
-		font.draw(batch, String.valueOf(score), 100 , 200);
-
-		batch.end();
-
-		birdcircle.set(Gdx.graphics.getWidth()/2,birdY+birds[flapState].getHeight()/2 , birds[flapState].getWidth()/2);
-		// shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-		//shapeRenderer.setColor(Color.RED);
-		//shapeRenderer.circle(birdcircle.x,birdcircle.y, birdcircle.radius);
-
-		for (int i = 0; i< numberOfTubes; i++)
-		{
-			// shapeRenderer.rect(tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i], topTube.getWidth(),topTube.getHeight());
-			//shapeRenderer.rect(tubeX[i],Gdx.graphics.getHeight() / 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i],bottomTube.getWidth(),bottomTube.getHeight());
-			if(Intersector.overlaps(birdcircle,topTubeRectangle[i])|| Intersector.overlaps(birdcircle,bottomTubeRectangles[i]))
-			{
-				gameState= 2;
 			}
+
+			if (flapState == 0) {
+				flapState = 1;
+			} else {
+				flapState = 0;
+			}
+
+			//create flapping effect
+			batch.draw(birds[flapState], Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2, birdY);
+			font.draw(batch, String.valueOf(score), 100, 200);
+
+			batch.end();
+
+			birdcircle.set(Gdx.graphics.getWidth() / 2, birdY + birds[flapState].getHeight() / 2, birds[flapState].getWidth() / 2);
+			// shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+			//shapeRenderer.setColor(Color.RED);
+			//shapeRenderer.circle(birdcircle.x,birdcircle.y, birdcircle.radius);
+
+			for (int i = 0; i < numberOfTubes; i++) {
+				//shapeRenderer.rect(tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i], topTube.getWidth(),topTube.getHeight());
+				//shapeRenderer.rect(tubeX[i],Gdx.graphics.getHeight() / 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i],bottomTube.getWidth(),bottomTube.getHeight());
+				if (Intersector.overlaps(birdcircle, topTubeRectangle[i]) || Intersector.overlaps(birdcircle, bottomTubeRectangles[i])) {
+					gameState = 2;
+				}
+			}
+			//shapeRenderer.end();
 		}
-		//shapeRenderer.end();
-	}
 
 }
